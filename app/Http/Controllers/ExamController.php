2d96fb06409d542e65534610ac3fa24c40ffc759
koldaf <koldaf20@gmail.com>
2022-08-28 20:06:44 +0100

    Working on processing test score

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\QuestionBank;
+use App\Models\User;
 use App\Models\Question;
-use App\Models\QuestionInstance;
-use App\Models\QuestionUserResponse;
+use App\Models\QuestionBank;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
+use App\Models\QuestionInstance;
+use Illuminate\Support\Facades\DB;
+use App\Models\QuestionUserResponse;
 use Rap2hpoutre\FastExcel\FastExcel;
+use Illuminate\Support\Facades\Storage;
 
 class ExamController extends Controller
 {
@@ -43,7 +45,6 @@ class ExamController extends Controller
                     ->get();
         $exam_detail =QuestionBank::where('id', $exam)->first();
 
-        //dd($exam_detail->toArray());
 
         return view('pages.edit-exams',[
             'questions' =>$questions->toArray(), // $exam->all()->toArray(),
@@ -53,24 +54,52 @@ class ExamController extends Controller
     }
 
     public function view_scores($exam){
-
-
-        //dd($exam_detail->toArray());
+        $exam = QuestionBank::where('id', $exam)
+                            ->first()
+                            ->get()
+                            ->toArray();
+        //get list participants and their scores
+        //participants
+        $all = User::all();
+        foreach ($all as $user){
+            //get score for each users
+           //dd($user->id);
+        }
+        dd($this->calculate_score(4, 1));
+        //array('user'=>x, 'score'=>y)
 
         return view('pages.view-score',[
-            'user' => auth()->user()
+            'user' => auth()->user(),
+            'exam' => $exam
         ]);
     }
 
-    public function calculate_score($qid, $resp){
-        $check = Question::where('id', $qid)
-                          ->where('correct_answer', $resp)
-                          ->count();
-        if($check > 0){
-            return true;
-        }else{
-            return false;
-        }
+
+    public function calculate_score($userid, $instance){
+        // $grade = DB::table('question_user_responses')
+        //             ->join('questions','question_user_responses.question_id','=','questions.id')
+        //              ->where('question_user_responses.response', 'questions.correct_answer')
+        //              ->where('question_user_responses.instance_id', $instance)
+        //              ->where('question_user_responses.user_id', $userid)
+        //             ->count();
+        // $grade = DB::table('question_user_responses')
+        //     ->join('questions', function($join) use($instance, $userid){
+        //         $join->on('question_user_responses.question_id','=','questions.id')
+        //                 ->where('question_user_responses.response', 'questions.correct_answer')
+        //                 ->where('question_user_responses.instance_id', $instance)
+        //                 ->where('question_user_responses.user_id', $userid);
+        //     })
+        //     ->count();
+
+           $grade = DB::Raw("SELECT * FROM question_user_responses AS qua INNER JOIN questions AS q ON qua.question_id = q.id  WHERE qua.instance_id = '1' AND qua.user_id = '4' AND qua.response = q.correct_answer"); //->count();
+
+                    // ->join('contacts', function ($join) {
+                    //     $join->on('users.id', '=', 'contacts.user_id')
+                    //          ->where('contacts.user_id', '>', 5);
+                    // })
+                    //SELECT * FROM question_user_responses AS qua INNER JOIN questions AS q ON qua.question_id = q.id  WHERE qua.instance_id = '1' AND qua.user_id = '4' AND qua.response = q.correct_answer
+
+        return $grade;
 
     }
 
--- resources/views/pages/view-score.blade.php
+++ resources/views/pages/view-score.blade.php
@@ -5,7 +5,7 @@
     <nav>
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{ url('/dashboard') }}">Home</a></li>
-        <li class="breadcrumb-item active">{{ $exam['exam_title'] }}</li>
+        <li class="breadcrumb-item active">{{ $exam[0]['exam_title'] }}</li>
       </ol>
     </nav>
   </div><!-- End Page Title -->
@@ -13,7 +13,7 @@
   <section class="section">
     <div class="card col-8 col-xs-10">
         <div class="card-body">
-            <h3 class="card-title">{{ $exam['exam_title'] }}</h3>
+            <h3 class="card-title">{{ $exam[0]['exam_title'] }}</h3>
             @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -32,26 +32,14 @@
             <thead>
                 <tr>
                     <th>#</th>
-                    <th style="width:60%;">Questions</th>
-                    <th style="text-align:center" colspan="2" >Actions</th>
+                    <th style="width:60%;">Participants</th>
+                    <th style="text-align:center" colspan="2" >Score</th>
                 </tr>
             </thead>
             <tbody>
                 @php $i = 0 @endphp
 
-                @foreach ($questions as $question )
-                <tr>
-                    <td> {{ ++$i }} </td>
-                    <td>{{ $question['question'] }}</td>
-                    <td><a href='{{ url('/edits/'.$question['id']) }}' class='btn btn-primary'><i class="bi bi-pencil-square"></i> Edit </a></td>
-                    <td><form method="POST" action="{{ url('/delete-quiz/'.$question['id']) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger"><i class="bi bi-trash"></i> Delete</button>
-                    </form></td>
-                </tr>
 
-                @endforeach
             </tbody>
         </table>
 
